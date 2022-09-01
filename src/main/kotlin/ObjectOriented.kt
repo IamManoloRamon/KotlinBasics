c18ced778cb54ab22720d8ade2fcde0807f58e8f
Manolo Ramon Manatlao <manolo-ramon@outlook.com>
2022-09-01 13:11:50 +0400

    init block

--- src/main/kotlin/ObjectOriented.kt
+++ src/main/kotlin/ObjectOriented.kt
@@ -2,14 +2,23 @@
 fun main(args: Array<String>) {
     // intro to class and constructor
     val car = Car(color = "Green", model = "FMH")
-
+    val secondCar = Car(color = "Brown", "AAA")
     println("Car color is ${car.color} model: ${car.model}")
+    println("Car color is ${secondCar.color} model: ${secondCar.model}")
 
-    //TODO: The init block
+
+    //TODO: 4. Adding a class Function with Parameters
 }
 
-class Car(var color: String = "Red", var model: String = "XMD") {
-
+class Car(var color: String, var model: String) {
+    //The init block
+    init {
+       if (color == "Green") {
+           println("Color is Green")
+       } else {
+           println("$color is not green")
+       }
+    }
 
     fun drive(){
         println("Drive....")
